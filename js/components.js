b419076406e2f3d4bb7a8bfeef856866caa900f3
hirshtom-web <hirshtom@gmail.com>
2026-08-11 14:24:46 +0300

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,1 +1,32 @@
-x
+async function loadComponent(id, file) {
+    const element = document.getElementById(id);
+
+    if (!element) return;
+
+    try {
+        const response = await fetch(file);
+
+        if (!response.ok) {
+            throw new Error(`Failed to load ${file}`);
+        }
+
+        element.innerHTML = await response.text();
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadComponent(
+        "site-header",
+        "components/header.html"
+    );
+
+    loadComponent(
+        "site-footer",
+        "components/footer.html"
+    );
+
+});
